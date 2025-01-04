dca9899497a0eaf53a8a7dac423bc9474d7a9bb6
Philipp Mundhenk <philipp@mundhenk.org>
2025-01-04 17:21:22 +0100

    minimized list

--- list.php
+++ list.php
@@ -7,9 +7,7 @@
 <div class="divTable" style="width: 100%;border: 1px solid #000;" >
 <div class="divTableBody">
 <div class="divTableRow">
-<div class="divTableCell">Set number</div>
-<div class="divTableCell">Set image</div>
-<div class="divTableCell">Set name</div>
+<div class="divTableCell">Set</div>
 <div class="divTableCell">Instructions</div>
 </div>
 
@@ -20,10 +18,8 @@ $locale="de-de";
 $items = glob($directory . '/*');
 foreach ($items as $item) {
         $set_id=0;
-        echo '<div class="divTableRow">'."\n";
         if (is_dir($item)) {
                 $set_id=str_replace("/downloads/", "", $item);
-                echo "<div class='divTableCell'>{$set_id}</div>\n";
         }
         $json="";
         $files = glob($item . '/*');
@@ -33,11 +29,10 @@ foreach ($items as $item) {
                 }
         }
 
+        echo '<div class="divTableRow">';
         echo '<div class="divTableCell">';
         echo "<img src='".$item."/".basename($json["hits"]["hits"][0]["_source"]["assets"][0]["assetFiles"][0]["url"])."' />";
-        echo "</div>\n";
-
-        echo '<div class="divTableCell">';
+        echo "<br/><br/>";
         echo $json["hits"]["hits"][0]["_source"]["locale"][$locale]["display_title"];
         echo "</div>\n";
 
